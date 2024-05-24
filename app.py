6241062d701e1d64e76677ee74a3adf7addcf053
5522079 <5522079h@st.toho-u.jp>
2024-05-24 21:27:39 +0900

    予測データの説明文を保存

--- app.py
+++ app.py
@@ -9,13 +9,20 @@ def load_log():
         reader = csv.reader(csvfile)
         logs = list(reader)
         date = str(logs[0][0])
-        converted_date = re.sub(r'\d{4}-(\d{2})-(\d{2}) \d{2}:\d{2}:\d{2}', lambda x: f"{int(x.group(1))}月{int(x.group(2))}日", date)
-        return converted_date
+        update_date = re.sub(r'\d{4}-(\d{2})-(\d{2}) \d{2}:\d{2}:\d{2}', lambda x: f"{int(x.group(1))}月{int(x.group(2))}日", date)
+        return update_date
+
+def load_comment():
+    with open('data/comment.txt', 'r', encoding='utf-8') as txtfile:
+        comment = txtfile.read()
+        return comment
+
 
 @app.route('/')
 def index():
-    date_log = load_log()
-    return render_template('index.html', date=date_log)
+    update_date = load_log()
+    comment = load_comment()
+    return render_template('index.html', date=update_date, content=comment)
 
 if __name__ == '__main__':
     app.run(debug=True)
